bbf88a27755c896e166b7a304ea1b142d67fc2f7
PoissonPoisson <romain.delorme-glorieux@sfr.fr>
2020-05-04 10:27:23 +0200

    Create Teacher component

---
+++ frontend/src/views/TeacherHomepage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="teacher">
+    <nav>
+      <ul>
+        <li><button>Mes étudiants</button></li>
+        <li><button>Projets</button></li>
+        <li><button>Technologieq</button></li>
+      </ul>
+      <button>Déconnexion</button>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeacherHomepage'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
