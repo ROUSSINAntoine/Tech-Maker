09c07379a4690986b8e54950eb5c3b65913eae9f
PoissonPoisson <romain.delorme-glorieux@sfr.fr>
2020-05-15 22:51:59 +0200

    Hide not implemented feature for teacher.

--- frontend/src/components/teacher/TeacherNavbar.vue
+++ frontend/src/components/teacher/TeacherNavbar.vue
@@ -2,7 +2,7 @@
   <div id='TeacherNavbar'>
     <nav>
       <ul>
-        <li><router-link :to='"/teacher/" + $route.params.id +  "/students"'><button>Mes étudiants</button></router-link></li>
+        <!--li><router-link :to='"/teacher/" + $route.params.id +  "/students"'><button>Mes étudiants</button></router-link></li-->
         <li><router-link :to='"/teacher/" + $route.params.id +  "/projects"'><button>Projets</button></router-link></li>
         <li><router-link :to='"/teacher/" + $route.params.id +  "/technologies"'><button>Technologies</button></router-link></li>
       </ul>
