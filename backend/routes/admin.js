ab0d0802b5c2ca87203c2504d484ce605629d495
Antoine ROUSSIN <antoine.roussin.lefevre@gmail.com>
2020-06-12 15:56:46 +0200

    start pdf

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -14,6 +14,7 @@ const Judge = require('../models/judge.model.js');
 const Jury = require('../models/jury.model');
 const Project = require('../models/project.model.js');
 const Room = require('../models/room.model.js');
+const RenderPDF = require('chrome-headless-render-pdf');
 
 router.get('/techno', async function (req, res, next) {
   const semesters = await Semester.getAllNamesIds();
@@ -174,5 +175,15 @@ router.put('/rooms/update', async (req, res) => {
 
   res.status(200).send({ error: false, message: 'Opération effectuée avec succès.' });
 });
+
+router.post('/juryCSV', async function (req, res, next) {
+  // RenderPDF.default.
+  RenderPDF.default.generateSinglePdf('http://google.com', 'outputPdf.pdf', {chromeBinary : 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'});
+  res.send({ response: 'Les étudiants ont bien été ajouté à la base de donnée.' });
+});
+
+router.get('pdf', async function (req, res, next) {
+  RenderPDF.default.generateSinglePdf(``, `${__dirname}/../pdf/${req.body.name}.pdf`)
+})
 
 module.exports = router;
